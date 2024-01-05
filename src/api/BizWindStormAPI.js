e4c11118f820ffb2f0da5fcd7a3643fbae871a2f
seohanna <alrzl4302@naver.com>
2024-01-05 09:46:05 +0900

    프록시 추가

--- src/api/BizWindStormAPI.js
+++ src/api/BizWindStormAPI.js
@@ -11,9 +11,6 @@ let tokenConfig = {
   headers: { 
     "Content-Type": `application/json;charset=UTF-8`,
     'Accept': 'application/json',
-    // "Access-Control-Allow-Origin": "*",
-    // 'Access-Control-Allow-Credentials':"true",
-    withCredentials: true
   },
   data : data
 };
@@ -23,9 +20,6 @@ export const bizWindstormAPI = axios.create({
   headers: {
     "Content-Type": `application/json;charset=UTF-8`,
     "Accept": "application/json",
-    // "Access-Control-Allow-Origin": "*",
-    // 'Access-Control-Allow-Credentials':"true",
-    withCredentials: true
   }
 });
 
@@ -43,9 +37,6 @@ export const MoneypinBizInfo = async (bizNoList, token) => {
       "Content-Type": `application/json;charset=UTF-8`, 
       'Accept': 'application/json',
       'Authorization': `Bearer ${token}`,
-      // "Access-Control-Allow-Origin": "*",
-      // 'Access-Control-Allow-Credentials':"true",
-      withCredentials: true
     },
     data: {
       "bizNoList": [bizNoList]
