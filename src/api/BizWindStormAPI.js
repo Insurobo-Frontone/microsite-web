19425b507cd0989f709b503ac7612ee6900a7e3f
Keunreol Park <ritter4u@gmail.com>
2024-01-06 01:32:18 +0900

    Change to getTicket

--- src/api/BizWindStormAPI.js
+++ src/api/BizWindStormAPI.js
@@ -1,19 +1,19 @@
 import axios from "axios";
-let data = JSON.stringify({
-  "grantType": "ClientCredentials",
-  "clientId": "372a1d0b-99c2-45f4-914c-a5ad5045bbfb",
-  "clientSecret": "jIFmTlj/cIwWCJ+TazcG3OwixyLPRCdiAGDfIluP718="
-});
-let tokenConfig = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: 'https://api.moneypin.biz/bizno/v1/auth/token',
-  headers: { 
-    "Content-Type": `application/json;charset=UTF-8`,
-    'Accept': 'application/json',
-  },
-  data : data
-};
+//let data = JSON.stringify({
+//  "grantType": "ClientCredentials",
+//  "clientId": "372a1d0b-99c2-45f4-914c-a5ad5045bbfb",
+//  "clientSecret": "jIFmTlj/cIwWCJ+TazcG3OwixyLPRCdiAGDfIluP718="
+//});
+//let tokenConfig = {
+//  method: 'post',
+//  maxBodyLength: Infinity,
+//  url: 'https://api.moneypin.biz/bizno/v1/auth/token',
+//  headers: {
+//    "Content-Type": `application/json;charset=UTF-8`,
+//    'Accept': 'application/json',
+//  },
+//  data : data
+//};
 
 export const bizWindstormAPI = axios.create({
   baseURL: 'https://insurobo.com',
@@ -25,7 +25,7 @@ export const bizWindstormAPI = axios.create({
 
 // 머니핀 인증토큰 생성
 export const MoneypinToken = async () => {
-  return await axios(tokenConfig)
+  return await axios('https://insurobo.com:5000/getTicket')
 }
 
 export const MoneypinBizInfo = async (bizNoList, token) => {
