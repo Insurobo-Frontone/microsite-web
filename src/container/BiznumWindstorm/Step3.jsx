c346874d3b047293fff4afa0dcffc8f12cf99dc6
seohanna <alrzl4302@naver.com>
2024-01-23 09:54:17 +0900

    Update Step3.jsx

--- src/container/BiznumWindstorm/Step3.jsx
+++ src/container/BiznumWindstorm/Step3.jsx
@@ -86,7 +86,7 @@ const Step3 = ({ data }) => {
       }).catch((e) => console.log(e))
     } else {
       setValue('zipcode', data?.zipCode)
-      console.log(watch('zipcode'))
+      // console.log(watch('zipcode'))
     }
   }, [data, jehuCd]);
 
